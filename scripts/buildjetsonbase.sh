ebfc715a027511585a6c2506f96248f16d5781e9
Dan Walkes <danwalkes@trellis-logic.com>
2024-04-21 14:54:59 -0600

    jetson: initial build support
    Not yet tested or working, but gets through the initial kas setup

---
+++ scripts/buildjetsonbase.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+base_repo_path=$(realpath $(dirname $0))/../
+cd ${base_repo_path}/..
+base=$(basename $0 .sh)
+machine=$(echo $base | cut -s -d '-' -f 2-)
+if [ ! -z "${machine}" ]; then
+    echo "building for machine ${machine}"
+    export KAS_MACHINE=${machine}
+fi
+kas build --update ${base_repo_path}/conf/kas/jetson-swupdate-aws.yml
